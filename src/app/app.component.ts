e7ad9b6b925c652098f033121e199103d344f5b3
David Azdejkovic <noxdavid@elfak.rs>
2022-03-16 11:58:20 +0100

    Prodavnica_hrane

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'shop';
+ 
+  loadedFeature = 'recipe';
+
+  onNavigate(feature:string)
+  {
+    this.loadedFeature = feature;
+  }
 }
